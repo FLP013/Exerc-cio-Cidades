0e2f315efd419505871d63138a14e3ffee832a24
FLP013 <felliperibeiro9@gmail.com>
2024-09-02 20:07:21 -0300

    programa finalizado

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.WriteLine("Cananeia");
+Console.WriteLine("Ilha Comprida");
+Console.WriteLine("Iguape");
+Console.WriteLine("Peruíbe");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("São Vicente");
+Console.WriteLine("Cubatão");
+Console.WriteLine("Santos");
+Console.WriteLine("Guarujá");
+Console.WriteLine("Bertioga");
